0ff12a9882b46a7ade43677392540fd3771711bb
chris <chris@lazarus.westnet.com>
2011-11-06 15:05:41 -0500

    Reactivate Doreen's email address.

--- pnuserapi.php
+++ pnuserapi.php
@@ -141,8 +141,8 @@ function School_userapi_MailFormUpdated($args)
     $obj = DBUtil::selectObjectByID('School_family', $familyid, 'id', array('LastName'));
     $familyname = $obj['LastName'];
     $subject = "$formname form updated by $familyname";
-//     $toaddress= array('Doreenp@resurrectionschool.com'); //, 'chris@westnet.com');
-    $toaddress= array( 'chris@westnet.com');
+    $toaddress= array('Doreenp@resurrectionschool.com', 'chris@westnet.com');
+    // $toaddress= array( 'chris@westnet.com');
     
     $mail = "\nThe \"$familyname\" family ($familyid) has updated their $formname form.\n\n";
     pnModAPIFunc('Mailer', 'user', 'sendmessage',
