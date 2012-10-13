8cc99b24c28c6a1922bb0e967a2ef203511f59d2
Christopher X. Candreva <chris@westnet.com>
2012-10-13 16:07:24 -0400

    Add LAtestAlumniClass setting.

--- pnclass/confighandler.php
+++ pnclass/confighandler.php
@@ -2,7 +2,7 @@
 class School_user_configHandler extends pnFormHandler
 {
 
-  var $fields = array('ThisYear', 'NextYear', 'NextYearStart', 'EnrollStart', 'ReregOpen');
+  var $fields = array('ThisYear', 'NextYear', 'NextYearStart', 'EnrollStart', 'ReregOpen', 'LatestAlumniClass');
   
   function initialize(&$render)
   {
