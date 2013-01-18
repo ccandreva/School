9e9dd1e7c345378cda7ff9f664bb2cf0c4f9c656
Christopher X. Candreva <chris@westnet.com>
2013-01-18 17:36:42 -0500

    Generalize family search so it can work with any page.

--- pnjavascript/school.js
+++ pnjavascript/school.js
@@ -2,17 +2,17 @@
  * Zikula Application Framework
  *
 */
+var entrypoint;
 
 function livefamilysearch()
 {
     Element.removeClassName('livefamilysearch', 'z-hide');
-    Event.observe('searchfamily', 'click', function() { window.location.href=document.location.entrypoint + "?module=School&type=admin&func=emergencysearch&familyname=" + $F('familyname');}, false);
+    Event.observe('searchfamily', 'click', function() { window.location.href=document.location.entrypoint + entrypoint + $F('familyname');}, false);
     new Ajax.Autocompleter('familyname', 'familyname_choices', document.location.pnbaseURL + 'ajax.php?module=School&func=getfamilies',
        {paramName: 'fragment',
         minChars: 3,
         afterUpdateElement: function(data){
-            // Event.observe('searchfamily', 'click', function() { window.location.href=document.location.entrypoint + "?module=School&type=admin&func=emergencysearch&familyname=" + $($(data).value).value;}, false);
-            Event.observe('searchfamily', 'click', function() { window.location.href=document.location.entrypoint + "?module=School&type=admin&func=emergencysearch&familyname=" + $F('familyname');}, false);
+            Event.observe('searchfamily', 'click', function() { window.location.href=document.location.entrypoint + entrypoint + $F('familyname');}, false);
             }
         }
     );
